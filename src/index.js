f643f06a3c77355b9f2aef3ad76fe06d0f207529
DiegoAbner <diego86abner@gmail.com>
2024-03-18 18:31:59 -0300

    Meu envio das respostas commitadas

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 // O que acontece ao tentar reatribuir um valor a const idade? ou seja, criar uma segunda const idade = 30?
 const idade = 25;
-//console.log(idade); 
+const idade = 30;
+console.log(idade); 
 
 
-
+// R: Vai dar erro. Variável "const" não pode ser retribuída
 
 
 ////////////////////////////////////////////////////////////////////////
@@ -16,11 +17,11 @@ const idade = 25;
 
 {
     let x = 10;
-   // console.log(x); 
+   console.log(x); 
 }
-// console.log(x);
+console.log(x);
 
-
+// R: O resultado de x será 10, dentro do escopo. Fora do escopo, dará erro porque essa variável não fora declarada. Console.log não enxerga o Let dentro do escopo.
 
 ///////////////////////////////////////////////////////////////////////
 
@@ -31,19 +32,20 @@ const idade = 25;
     var y = 20;
 }
 
-// console.log(x);
-// console.log(y);
+console.log(x);
+console.log(y);
 
+//R: No "console.log(x);"  vai dar erro porque é uma "let"e será interrompido o processo por não estar definida a variável. No "console.log(y);" a variável é var. E var é global, não é dependente do escopo.
 ///////////////////////////////////////////////////////////////////////
 
 //O que acontece nessa declaração, explique sobre o Hoisting
 
-//console.log(a); 
+console.log(a); 
 var a = 5;
 console.log(b);
 //let b = 10;
 
-
+//R: No console.Lo(a) ocorre o Hoisting da variável para o topo. No console.log(b) a variavel b não foi declarada
 /////////////////////////////////////////////////////////////////////////////////
 
 //No código abaixo apresenta um erro, qual?
@@ -56,9 +58,12 @@ let y = 15;
 let y = 20; 
 // console.log(y); 
 
+//R: A variável "Y" é um "let", e não permite redeclaração. Portanto, dará erro.
 
 //// Escreva um programa que print a primeira letra dessa variavel 
-let nomeCompleto = 'eduardo'
+let nomeCompleto = 'eduardo';
+const letraInicial = nomeCompleto[0];
+console.log(letraInicial);
 
 //sua lógica para pegar a primeira letra 
 /// const letraInicial = ...
